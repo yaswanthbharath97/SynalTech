0385df4e6fe70f8e514da73c6a77cd2172cdcfd7
yaswanthbharath97 <yaswanthbharath97@gmail.com>
2023-04-18 17:51:06 +0530

    project

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,15 +2,23 @@ package com.example.myapplication;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+
 import android.Manifest;
 
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -26,51 +34,107 @@ public class MainActivity extends AppCompatActivity {
     Button startButton;
 
     BluetoothAdapter bluetoothAdapter;
+    BluetoothManager bluetoothManager;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         startButton = (Button) findViewById(R.id.buttonOn);
-        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        bluetoothManager=(BluetoothManager)getApplicationContext().getSystemService(Activity.BLUETOOTH_SERVICE);
+        bluetoothAdapter =bluetoothManager.getAdapter();
 
+        // Check for ACCESS_FINE_LOCATION permission
+        if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // Request the permission
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_PERMISSION);
+        } else {
+            // Permission has already been granted, so enable/disable Bluetooth
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                enableDisable();
+
+            }
+        }
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                enableDisable();
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                    enableDisable();
+                    Intent qrIntent=new Intent(MainActivity.this,QRCode_Scanner.class);
+                    startActivity(qrIntent);
+                }
             }
         });
 
 
     }
 
+    private final BroadcastReceiver mBroadcastReceiver1 = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals(BluetoothAdapter.ACTION_STATE_CHANGED)) {
+                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR);
+                switch (state) {
+                    case BluetoothAdapter.STATE_OFF -> Log.d(TAG, "OnReceive:STATE_OFF");
+                    case BluetoothAdapter.STATE_TURNING_OFF ->
+                            Log.d(TAG, "OnReceive:STATE_TURNING_OFF");
+                    case BluetoothAdapter.STATE_ON -> Log.d(TAG, "OnReceive:STATE_ON");
+                    case BluetoothAdapter.STATE_TURNING_ON ->
+                            Log.d(TAG, "OnReceive:STATE_TURNING_ON ");
+                }
+
+            }
+        }
+    };
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (isReceiverRegistered(mBroadcastReceiver1)) {
+            unregisterReceiver(mBroadcastReceiver1);
+        }
+    }
+
+    private boolean isReceiverRegistered(BroadcastReceiver receiver) {
+        try {
+            IntentFilter filter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+            return registerReceiver(receiver, filter) != null;
+        }
+        catch (Exception e) {
+            return false;
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.S)
     private void enableDisable() {
         if (bluetoothAdapter == null) {
             Log.d(TAG, "enableDisable:Does not Have Bt capabilities");
+            Toast.makeText(this, "This device does not support Bluetooth", Toast.LENGTH_SHORT).show();
             return;
 
         }
         if (!bluetoothAdapter.isEnabled()) {
             Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            if (ActivityCompat.checkSelfPermission(MainActivity.this,Manifest.permission.BLUETOOTH) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.BLUETOOTH}, REQUEST_PERMISSION);
-
+            if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.BLUETOOTH_CONNECT}, REQUEST_PERMISSION);
                 return;
             }
-            startActivityForResult(enableIntent,REQUEST_ENABLE_BT);
-            IntentFilter BtIntent=new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
-             registerReceiver(mBroadcastReceiver1,BtIntent);
-        }
-        else {
-            bluetoothAdapter.disable();
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+            IntentFilter BtIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+            registerReceiver(mBroadcastReceiver1, BtIntent);
         }
     }
+
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == REQUEST_PERMISSION) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                enableDisable();
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                    enableDisable();
+                }
             } else {
                 Toast.makeText(MainActivity.this, "Bluetooth permission is required to enable Bluetooth", Toast.LENGTH_SHORT).show();
             }
@@ -80,14 +144,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==REQUEST_ENABLE_BT)
-        {
-            if(resultCode==RESULT_OK)
-            {
-                Log.d(TAG,"onActivityResult:BT enabled");
-            }else
-            {
-                Log.d(TAG,"onActivityResult:Bt not enabled");
+        if (requestCode == REQUEST_ENABLE_BT) {
+            if (resultCode == RESULT_OK) {
+                Log.d(TAG, "onActivityResult:BT enabled");
+            } else {
+                Log.d(TAG, "onActivityResult:Bt not enabled");
             }
         }
     }
---
+++ app/src/main/java/com/example/myapplication/QRCode_Scanner.java
@@ -0,0 +1,206 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
+import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.CameraDevice.StateCallback;
+
+import android.Manifest;
+import android.os.Bundle;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.Toast;
+
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.MultiProcessor;
+import com.google.android.gms.vision.Tracker;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.util.Arrays;
+
+
+public class QRCode_Scanner extends AppCompatActivity implements SurfaceHolder.Callback, Detector.Processor<Barcode> {
+
+    private static final int CAMERA_PERMISSION_REQUEST_CODE = 100;
+
+    private CameraManager cameraManager;
+    private CameraDevice cameraDevice;
+    private CaptureRequest.Builder captureRequestBuilder;
+    private SurfaceView surfaceView;
+    private BarcodeDetector barcodeDetector;
+
+    private boolean cameraPermissionGranted = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrcode_scanner);
+
+        surfaceView = findViewById(R.id.surface_view);
+        barcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.QR_CODE).build();
+
+        barcodeDetector.setProcessor(new MultiProcessor.Builder<>(new QRCodeTrackerFactory()).build());
+        SurfaceHolder surfaceHolder = surfaceView.getHolder();
+        surfaceHolder.addCallback(this);
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            // Check if the permission is granted
+            if (grantResults.length > 0
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                cameraPermissionGranted = true;
+                // Permission is granted, setup the camera
+                setupCamera();
+            } else {
+                // Permission is denied, show a message or take action accordingly
+                Toast.makeText(this, "Camera permission is required to use this feature", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        if (ActivityCompat.checkSelfPermission(QRCode_Scanner.this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED) {
+            // Permission is granted, setup the camera
+            setupCamera();
+
+        }
+    }
+
+    private void setupCamera() {
+        // Add your camera setup code here
+        cameraManager = (CameraManager) getApplicationContext().getSystemService(Context.CAMERA_SERVICE);
+        try {
+            String cameraId = cameraManager.getCameraIdList()[0];
+
+            if (ActivityCompat.checkSelfPermission(QRCode_Scanner.this, Manifest.permission.CAMERA)
+                    != PackageManager.PERMISSION_GRANTED) {
+                // Permission is not granted, request it
+                ActivityCompat.requestPermissions(QRCode_Scanner.this,
+                        new String[]{Manifest.permission.CAMERA},
+                        CAMERA_PERMISSION_REQUEST_CODE);
+
+
+            } else {
+                // Permission is granted, setup the camera
+                cameraPermissionGranted = true;
+
+            }
+
+            cameraManager.openCamera(cameraId, new CameraDevice.StateCallback() {
+                @Override
+                public void onOpened(@NonNull CameraDevice camera) {
+                    cameraDevice = camera;
+                    try {
+                        captureRequestBuilder = camera.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+                        captureRequestBuilder.addTarget(surfaceView.getHolder().getSurface());
+                        camera.createCaptureSession(Arrays.asList(surfaceView.getHolder().getSurface()), new CameraCaptureSession.StateCallback() {
+                            @Override
+                            public void onConfigured(@NonNull CameraCaptureSession session) {
+                                try {
+                                    session.setRepeatingRequest(captureRequestBuilder.build(), null, null);
+
+                                } catch (Exception e) {
+                                    throw new RuntimeException(e);
+                                }
+
+                            }
+
+                            @Override
+                            public void onConfigureFailed(@NonNull CameraCaptureSession session) {
+
+
+                            }
+                        }, null);
+                    } catch (CameraAccessException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                }
+
+                @Override
+                public void onDisconnected(@NonNull CameraDevice camera) {
+
+                }
+
+                @Override
+                public void onError(@NonNull CameraDevice camera, int error) {
+
+                }
+            }, null);
+
+        } catch (CameraAccessException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        // Add your code here
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+        // Add your cleanup code here
+        if (cameraDevice != null) {
+            cameraDevice.close();
+            cameraDevice = null;
+        }
+    }
+
+    @Override
+    public void release() {
+
+
+    }
+
+    @Override
+    public void receiveDetections(@NonNull Detector.Detections<Barcode> detections) {
+        SparseArray<Barcode>barcodes=detections.getDetectedItems();
+        if (barcodes.size() > 0) {
+            onBarcodeScanned(barcodes.valueAt(0));
+        }
+
+    }
+
+    private void onBarcodeScanned(Barcode valueAt) {
+    }
+
+    private class QRCodeTrackerFactory implements MultiProcessor.Factory<Barcode> {
+        @Override
+        public Tracker<Barcode> create(Barcode barcode) {
+            return new QRCodeTracker();
+        }
+    }
+
+    private class QRCodeTracker extends Tracker<Barcode> {
+        @Override
+        public void onUpdate(Detector.Detections<Barcode> detections, Barcode barcode) {
+            onBarcodeDetected(barcode);
+        }
+        public void onBarcodeDetected(Barcode barcode) {
+            // Add your code here to handle the detected barcode
+        }
+    }
+
+    }
+
